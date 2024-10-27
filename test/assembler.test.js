351bf153c610cefde283795bad04c41104888caf
avidrucker <avidrucker@gmail.com>
2024-10-27 11:17:54 -0400

    fix test to match command line usage

--- test/assembler.test.js
+++ test/assembler.test.js
@@ -5,6 +5,10 @@ const Assembler = require('../src/assembler');
 
 function testAssembler() {
   const assembler = new Assembler();
+  
+  // Simulate command line arguments
+  process.argv = ['node', 'assembler.js', 'a1test.a'];
+  
   assembler.main();
 }
 
