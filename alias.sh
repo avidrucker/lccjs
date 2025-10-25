fc35f524e2fe1d21d79b3939ac437726eeaa976d
avidrucker <avidrucker@gmail.com>
2025-10-25 09:19:46 -1000

    update add aliases for hex display and picture utils

--- alias.sh
+++ alias.sh
@@ -2,11 +2,13 @@
 
 LCC_JS="lccjs"
 LCC_PLUS_JS="lccplusjs"
+HEX_DISPLAY="hex"
+PICTURE="picture"
 
 LCC_JS_PATH="node $(pwd)/src/core/lcc.js"
 LCC_PLUS_JS_PATH="node $(pwd)/src/plus/lccplus.js"
-
-
+HEX_DISPLAY_PATH="node $(pwd)/src/utils/hexDisplay.js"
+PICTURE_PATH="node $(pwd)/src/utils/picture.js"
 
 if [[ $SHELL == *"zsh"* ]]; then # if the user is using zsh, then set the shell config to "~/.zshrc"
     SHELL_CONFIG_FILE="$HOME/.zshrc"
@@ -17,17 +19,27 @@ else # ADD SUPPORT FOR WINDOWS SHELLS LATER!!!
     exit 1
 fi
 
-if (! grep -q "alias $LCC_JS=" "$SHELL_CONFIG_FILE"); then # looks for the alias "lccjs" in the shell configuration file
-    CONSENT="N"
-    read -p "Do you consent to modifying the shell config file to add aliases for 'lccjs' and 'lccplusjs'? [y/N] " CONSENT
-    CONSENT=$(echo "$CONSENT" | tr '[:upper:]' '[:lower:]')
-    if [[ "$CONSENT" != "y" ]]; then
-        echo "Aliases not created"
-        exit 1
-    fi
-    echo "alias $LCC_JS='$LCC_JS_PATH'" >> "$SHELL_CONFIG_FILE" # adds the alias "lccjs" 
-    echo "alias $LCC_PLUS_JS='$LCC_PLUS_JS_PATH'" >> "$SHELL_CONFIG_FILE" # adds the alias "lccplusjs" to the shell config file
-    echo "Aliases '$LCC_JS' and '$LCC_PLUS_JS' added to $SHELL_CONFIG_FILE." # outputs the the user that the aliases have been successfully added
-else # if the aliases already exist...
-    echo "Aliases already exist in $SHELL_CONFIG_FILE. No changes made." # inform the user and move on
+# Check if any of the aliases already exist
+if (grep -q "alias $LCC_JS=" "$SHELL_CONFIG_FILE" && \
+    grep -q "alias $LCC_PLUS_JS=" "$SHELL_CONFIG_FILE" && \
+    grep -q "alias $HEX_DISPLAY=" "$SHELL_CONFIG_FILE" && \
+    grep -q "alias $PICTURE=" "$SHELL_CONFIG_FILE"); then
+    echo "One or more aliases already exist in $SHELL_CONFIG_FILE. No changes made."
+    exit 0
 fi
+
+# If no aliases exist, prompt for consent
+CONSENT="N"
+read -p "Do you consent to modifying the shell config file to add aliases for 'lccjs', 'lccplusjs', 'hex', and 'picture'? [y/N] " CONSENT
+CONSENT=$(echo "$CONSENT" | tr '[:upper:]' '[:lower:]')
+if [[ "$CONSENT" != "y" ]]; then
+    echo "Aliases not created"
+    exit 1
+fi
+
+# Add aliases to the shell config file
+echo "alias $LCC_JS='$LCC_JS_PATH'" >> "$SHELL_CONFIG_FILE" # adds the alias "lccjs" 
+echo "alias $LCC_PLUS_JS='$LCC_PLUS_JS_PATH'" >> "$SHELL_CONFIG_FILE" # adds the alias "lccplusjs" to the shell config file
+echo "alias $HEX_DISPLAY='$HEX_DISPLAY_PATH'" >> "$SHELL_CONFIG_FILE" # adds the alias "hex" to the shell config file
+echo "alias $PICTURE='$PICTURE_PATH'" >> "$SHELL_CONFIG_FILE" # adds the alias "picture" to the shell config file
+echo "Aliases '$LCC_JS', '$LCC_PLUS_JS', '$HEX_DISPLAY', and '$PICTURE' added to $SHELL_CONFIG_FILE." # outputs to the user that the aliases have been successfully added
